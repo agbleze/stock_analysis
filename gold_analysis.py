697fa3951ab35a685463bc020740046d8b7ec419
linus <lagbleze@st.ug.edu.gh>
2024-09-02 23:00:51 +0200

    visualize stcoks

--- gold_analysis.py
+++ gold_analysis.py
@@ -14,3 +14,34 @@ data = pd.read_csv(file_path)
 
 # %%
 px.line(data_frame=data, y="Close", x="Date", template="plotly_dark")
+
+#%%
+data
+# %%
+data.info()
+# %%
+data["Date"] = pd.to_datetime(data["Date"])
+# %%
+cmb_filepath = "/home/lin/codebase/stock_analysis/CBK.DE.csv"
+
+commerzebank_data = pd.read_csv(cmb_filepath)
+# %%
+px.line(data_frame=commerzebank_data, y="Low", x="Date", template="plotly_dark")
+# %%
+infineon_datapath = "/home/lin/codebase/stock_analysis/IFX.DE.csv"
+
+infneone_data = pd.read_csv(infineon_datapath)
+# %%
+px.line(data_frame=infneone_data, y="Low", x="Date", template="plotly_dark",
+        title="Infineion daily stocks")
+
+# %%
+
+bac_filepath = "/home/lin/codebase/stock_analysis/BAC.csv"
+
+bac_data = pd.read_csv(bac_filepath)
+px.line(data_frame=bac_data, y="Low", x="Date", template="plotly_dark",
+        title="Bank of America daily stocks")
+#%%
+(110/100)*31
+# %%
